57f2c70da82b385d4189abd19177563c5c9cdd31
Martin Mayer <m.mayer@eundc.at>
2025-04-08 08:58:24 +0200

    Limit Imports

--- EAF-IFC-direkt/importDirect.ps1
+++ EAF-IFC-direkt/importDirect.ps1
@@ -1,6 +1,9 @@
 # Just simply overwrite the previous import-db with an empty copy
 # cp .\eaf110120_0001-fresh .\eaf110120_0001
 
+#Maximum number of concurrent des.exe:
+$maxDes = 5
+
 echo $PSScriptRoot
 
 $source = "\_BXT_MP-KIB\"
@@ -16,10 +19,37 @@ $ifcs = Get-ChildItem -Path $ifcPath -Filter "*.ifc"
 
 #new-item -Path .\ -Name "importStraight.mac" -Force
 
+$desCountOld = 0
+
 foreach($ifc in $ifcs)
 {
+	
+	#current File:
 	$name = $ifc.Name.Replace($ifc.Extension, '')
 	
+	#don't allow too many des.exe
+	$des = Get-Process -Name des
+	$desCount = $des.Length
+	
+	while($desCount -ge $maxDes)
+	{
+		$des = Get-Process -Name des
+		$desCount = $des.Length
+		
+		#sleep loop:
+		sleep 1
+		
+		#display current count if changed
+		if($desCountOld -ne $desCount)
+		{
+			Write-Host "Current Number of Designs: " $desCount
+			Write-Host "Next in queue: " $name
+			$desCountOld = $desCount
+		}
+	}
+	
+	
+	#prepare IFC:
 	$cleanName = $name.replace(" ", "")
 	
 	$macName = $cleanName + ".mac"
@@ -38,6 +68,33 @@ foreach($ifc in $ifcs)
 	#$eLine = "`$M `"" + $macFull + "`""
 	#echo $eLine > .\$macName
 	
-	#Start-Process "C:\Program Files (x86)\EuC\Launcher 2.0\TTY\LauncherTTY.exe" -ArgumentList "--origin Server --setup PROD_EundC-E3D3.1 --softwareid DESI_310 --projectcode EAF --user ECADM --password EUNDC4600 --mdb /E_EC_Import_DBs --macropath $macFull"
-	Start-Process "C:\Program Files (x86)\EuC\Launcher 2.0\TTY\LauncherTTY.exe" -ArgumentList "--origin Local --setup PROD_EundC-E3D3.1 --softwareid DESI_310 --projectcode FUG --user ECADM --password EUNDC4600 --mdb /E_EC_Project_DBs --macropath $macFull"
+	$launcher = Get-Item "C:\Program Files (x86)\EuC\Launcher 2.0\TTY\LauncherTTY.exe"
+	
+	$arg = @(
+		"--origin Server"
+		"--setup PROD_EundC-E3D3.1" 
+		"--softwareid DESI_310" 
+		"--projectcode EAF"
+		"--user ECADM" 
+		"--password EUNDC4600"
+		"--mdb /E_EC_Import_DBs_11"
+		"--macropath $macFull"
+	)
+	
+	$argTest = @(
+		"--origin Local"
+		"--setup PROD_EundC-E3D3.1" 
+		"--softwareid DESI_310" 
+		"--projectcode FUG"
+		"--user ECADM" 
+		"--password EUNDC4600"
+		"--mdb /E_EC_Project_DBs"
+		"--macropath $macFull"
+	)
+	
+	Write-Host "Start Design, wait 5 seconds"
+	
+	Start-Process $launcher -ArgumentList $arg
+	
+	sleep 5
 }
