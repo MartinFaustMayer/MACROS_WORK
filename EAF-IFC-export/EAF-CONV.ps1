3e4c43e192854c5127b00f4f12cabe356ca36f61
Martin Mayer <m.mayer@eundc.at>
2025-07-30 14:08:34 +0200

    Minor Maintenance

--- EAF-IFC-export/EAF-CONV.ps1
+++ EAF-IFC-export/EAF-CONV.ps1
@@ -161,7 +161,7 @@ Compress-Archive -Path $stpPath* -DestinationPath .\Zips\$archName -Force
 # $netName = "https://cloud.eundc.at/remote.php/dav/files/5CF3411F-996C-4FB1-90EF-A02D3F0F8DD3/2024-017_PRI_EAF Voest Linz/034-Datenaustausch _ step%2Cifc%2C.../01_E%26C - PRI"
 # $netName = "https://cloud.eundc.at/remote.php/dav/files/5CF3411F-996C-4FB1-90EF-A02D3F0F8DD3/2024-017_PRI_EAF Voest Linz/034-Datenaustausch _ step%2Cifc%2Cetc/01_E%26C - PRI"
 $netName = "https://cloud.eundc.at/remote.php/dav/files/6289DF12-712A-4E4B-8FF7-D920E3EA769E/2024-017_PRI_EAF Voest Linz/034-Datenaustausch _ step%2Cifc%2Cetc/01_E%26C - PRI"
-net use l: $netName /user:m.mayer@eundc.at NewMexico2025!
+net use l: $netName /user:m.mayer@eundc.at NoMexico2025!
 
 echo 'Export date:' $currentDate 'changes since:' $lastUpdateString 'file Count:' $updated.Count '' >> changes.txt
 
